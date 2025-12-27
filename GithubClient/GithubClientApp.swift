a54b25b286fbd834758181d45781f019aec7dd2d
“Zaven <zavenhovhannisyan98@mail.ru>
2025-12-27 23:08:53 +0400

    Initial Commit

---
+++ GithubClient/GithubClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientApp.swift
+//  GithubClient
+//
+//  Created by Zaven Hovhannisyan on 27.12.25.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
